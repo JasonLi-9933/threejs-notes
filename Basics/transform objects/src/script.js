535ba42bf4f29a296b174a432da84aa7d18bd94f
Jason Li <haotian-li@outlook.com>
2023-01-24 19:47:10 -0800

    first commit

---
+++ Basics/transform objects/src/script.js
@@ -0,0 +1,89 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Group
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+const cube1 = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'blue' }));
+
+const cube2 = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'green'}));
+cube2.position.x = -2;
+
+const cube3 = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 'orange' }));
+cube3.position.x = 2;
+group.add(cube1, cube2, cube3);
+
+/*
+mesh.position.x = 0.7; //
+mesh.position.y = -0.6;
+mesh.position.z = 1;
+*/
+mesh.position.set(0.7, -0.6, 1);
+/*
+`position` inherit from Vector3 which has many methods
+console.log(mesh.position.length());
+console.log(mesh.position.normalize());
+console.log(mesh.position.distanceTo(new THREE.Vector3(1, 0, 5)));
+*/
+
+// Scale
+/*
+mesh.scale.x = 2;
+mesh.scale.y = 0.25;
+mesh.scale.z = 0.5;
+*/
+mesh.scale.set(2, 0.25, 0.5);
+
+// Rotation
+/*
+`rotation` is a Euler
+*/
+mesh.rotation.reorder('YXZ');
+mesh.rotation.z = Math.PI; // half rotation
+mesh.rotation.x = Math.PI/4;
+scene.add(mesh);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+// camera.lookAt(new THREE.Vector3(3,0,0))
+camera.lookAt(mesh.position); // rotate the camera so it faces the -z face of the target
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
